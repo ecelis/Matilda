4b8d33554f4de43c1f8c551f3cb213b3d0921cab
Ernesto Celis <ernesto@celisdelafuente.net>
2020-12-26 16:58:07 -0600

    Add vuetify

--- src/db.js
+++ src/db.js
@@ -4,7 +4,7 @@ import 'firebase/firestore';
 export const db = firebase.initializeApp({ projectId: process.env.VUE_APP_PROJECT_ID })
   .firestore();
 
-const { Timestamp, GeoPoint } = firbase.firestore;
+const { Timestamp, GeoPoint } = firebase.firestore;
 export { Timestamp, GeoPoint };
 
 db.settings({ timestampInSnapshots: true });
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import { firestorePlugin } from 'vuefire';
 import App from './App.vue';
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
 Vue.use(firestorePlugin);
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app');

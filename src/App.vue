b7edfed9cdde3ffbe14fff3f8abe3bfdc46066a0
Ernesto Celis <ernesto@celisdelafuente.net>
2021-01-03 21:49:00 -0600

    Add join room dialog

--- src/App.vue
+++ src/App.vue
@@ -14,9 +14,7 @@
           @error="onError" />
       </v-row>
       <v-row align="center" justify="space-around">
-        <v-btn elevation="2" color="primary"
-          @click="onJoin">Join room</v-btn>
-        <v-btn elevation="2" color="primary"
+        <v-btn v-if="onCall" elevation="2" color="primary"
           @click="onLeave">Hangup</v-btn>
       </v-row>
     </v-main>
@@ -24,6 +22,42 @@
     <v-footer app>
       <!-- TODO -->
     </v-footer>
+
+    <v-dialog
+      v-model="dialog"
+      persistent
+      max-width="600px">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn color="primary"
+        dark
+        v-bind="attrs"
+        v-on="on"
+        >
+        Join Room
+        </v-btn>
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="headline">Room</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field label="Room name"
+                required v-model="roomId" placeholder="public-room"></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn elevation="2" color="primary"
+          @click="onJoin">Enter</v-btn>
+          <v-btn text @click="dialog = false">Close</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-app>
 </template>
 
@@ -42,8 +76,10 @@ export default {
   },
   data() {
     return {
+      dialog: false,
+      onCall: false,
       img: null,
-      roomId: "public-room"
+      roomId: null
     }
   },
   methods: {
@@ -52,9 +88,12 @@ export default {
     },
     onJoin() {
       this.$refs.webrtc.join();
+      this.onCall = true;
+      this.dialog = false;
     },
     onLeave() {
       this.$refs.webrtc.leave();
+      this.onCall = false;
     },
     onError(error, stream) {
       console.log('On Error Event', error, stream);
